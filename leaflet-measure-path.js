e1a6c5bd0b0e3df4b01474959309b89288d03ea9
Per Liedman <per.liedman@dotnetmentor.se>
2016-09-23 16:12:13 +0200

    Update to 1.1.0

--- leaflet-measure-path.js
+++ leaflet-measure-path.js
@@ -1,11 +1,11 @@
-(function() {
+!(function() {
     'use strict';
     L.Icon.Measurement = L.DivIcon.extend({
         initialize: function(measurement, options) {
             L.Icon.prototype.initialize.call(this, L.extend({
                 className: 'leaflet-measure-path-measurement',
                 html: measurement,
-                iconSize: [36, 10]
+                iconSize: [50, 18]
             }, options));
         }
     });
@@ -20,6 +20,13 @@
             L.Marker.prototype.initialize.call(this, latLng, L.extend({
                 icon: icon
             }, options));
+        },
+
+        _setPos: function() {
+            L.Marker.prototype._setPos.apply(this, arguments);
+            if (this.options.rotation) {
+                this._icon.style.transform += ' rotate(' + this.options.rotation + 'rad)';
+            }
         }
     });
 
@@ -122,7 +129,7 @@
             area = area * RADIUS * RADIUS / 2;
         }
 
-        return area;
+        return Math.abs(area);
     };
 
     var circleArea = function circleArea(d) {
@@ -130,8 +137,19 @@
         return 2 * Math.PI * RADIUS * RADIUS * (1 - Math.cos(rho));
     };
 
-    var polylineSetLatLngs = L.Polyline.prototype.setLatLngs;
-    var polylineSpliceLatLngs = L.Polyline.prototype.spliceLatLngs;
+    var override = function(method, fn, hookAfter) {
+        if (!hookAfter) {
+            return function() {
+                method.apply(this, arguments);
+                fn.apply(this, arguments);
+            }
+        } else {
+            return function() {
+                fn.apply(this, arguments);
+                method.apply(this, arguments);
+            }
+        }
+    };
 
     L.Polyline.include({
         showMeasurements: function(options) {
@@ -141,7 +159,12 @@
                 showOnHover: false,
                 minPixelDistance: 30,
                 showDistances: true,
-                showArea: true
+                showArea: true,
+                lang: {
+                    totalLength: 'Total length',
+                    totalArea: 'Total area',
+                    segmentLength: 'Segment length'
+                }
             }, options || {});
 
             this._measurementLayer = L.layerGroup().addTo(this._map);
@@ -153,7 +176,7 @@
         },
 
         hideMeasurements: function() {
-            this._map.on('zoomend', this.updateMeasurements, this);
+            this._map.off('zoomend', this.updateMeasurements, this);
 
             if (!this._measurementLayer) return this;
             this._map.removeLayer(this._measurementLayer);
@@ -162,22 +185,34 @@
             return this;
         },
 
-        setLatLngs: function() {
-            polylineSetLatLngs.apply(this, arguments);
-            this.updateMeasurements();
-        },
+        onAdd: override(L.Polyline.prototype.onAdd, function() {
+            if (this.options.showMeasurements) {
+                this.showMeasurements(this.options.measurementOptions);
+            }
+        }),
 
-        spliceLatLngs: function() {
-            polylineSpliceLatLngs.apply(this, arguments);
+        onRemove: override(L.Polyline.prototype.onRemove, function() {
+            this.hideMeasurements();
+        }, true),
+
+        setLatLngs: override(L.Polyline.prototype.setLatLngs, function() {
             this.updateMeasurements();
-        },
+        }),
+
+        spliceLatLngs: override(L.Polyline.prototype.spliceLatLngs, function() {
+            this.updateMeasurements();
+        }),
 
         formatDistance: formatDistance,
         formatArea: formatArea,
 
         updateMeasurements: function() {
+            if (!this._measurementLayer) return;
+
             var latLngs = this.getLatLngs(),
                 isPolygon = this instanceof L.Polygon,
+                options = this._measurementOptions,
+                totalDist = 0,
                 formatter,
                 ll1,
                 ll2,
@@ -186,34 +221,56 @@
 
             this._measurementLayer.clearLayers();
 
-            if (this._measurementOptions.showDistances) {
+            if (this._measurementOptions.showDistances && latLngs.length > 1) {
                 formatter = this._measurementOptions.formatDistance || L.bind(this.formatDistance, this);
 
                 for (var i = 1, len = latLngs.length; (isPolygon && i <= len) || i < len; i++) {
                     ll1 = latLngs[i - 1];
                     ll2 = latLngs[i % len];
+                    dist = ll1.distanceTo(ll2);
+                    totalDist += dist;
+
                     pixelDist = this._map.latLngToLayerPoint(ll1).distanceTo(this._map.latLngToLayerPoint(ll2));
 
-                    if (pixelDist >= this._measurementOptions.minPixelDistance) {
-                        dist = ll1.distanceTo(ll2);
-                        L.marker.measurement([(ll1.lat + ll2.lat) / 2, (ll1.lng + ll2.lng) / 2], formatter(dist))
+                    if (pixelDist >= options.minPixelDistance) {
+                        L.marker.measurement(
+                            [(ll1.lat + ll2.lat) / 2, (ll1.lng + ll2.lng) / 2], 
+                            '<span title="' + options.lang.segmentLength + '">' + formatter(dist) + '</span>',
+                            L.extend({}, options, { rotation: this._getRotation(ll1, ll2)}))
                             .addTo(this._measurementLayer);
                     }
                 }
+
+                // Show total length for polylines
+                if (!isPolygon) {
+                    L.marker.measurement(ll2, '<strong title="' + options.lang.totalLength + '">' +
+                        formatter(totalDist) + '</strong>', options)
+                        .addTo(this._measurementLayer);
+                }
             }
 
-            if (isPolygon && this._measurementOptions.showArea) {
-                formatter = this._measurementOptions.formatArea || L.bind(this.formatArea, this);
+            if (isPolygon && options.showArea && latLngs.length > 2) {
+                formatter = options.formatArea || L.bind(this.formatArea, this);
                 var area = ringArea(latLngs);
-                L.marker.measurement(this.getBounds().getCenter(), formatter(area))
+                L.marker.measurement(this.getBounds().getCenter(),
+                    '<span title="' + options.lang.totalArea + '">' + formatter(area) + '</span>', options)
                     .addTo(this._measurementLayer);
             }
+        },
+
+        _getRotation: function(ll1, ll2) {
+            var p1 = this._map.project(ll1),
+                p2 = this._map.project(ll2);
+
+            return Math.atan((p2.y - p1.y) / (p2.x - p1.x));
         }
     });
 
-    var circleSetLatLng = L.Circle.prototype.setLatLng;
-    var circleSetRadius = L.Circle.prototype.setRadius;
-
+    L.Polyline.addInitHook(function() {
+        if (this.options.showMeasurements) {
+            this.showMeasurements();
+        }
+    });
 
     L.Circle.include({
         showMeasurements: function(options) {
@@ -242,31 +299,48 @@
             return this;
         },
 
-        setLatLng: function() {
-            circleSetLatLng.apply(this, arguments);
-            this.updateMeasurements();
-        },
+        onAdd: override(L.Circle.prototype.onAdd, function() {
+            if (this.options.showMeasurements) {
+                this.showMeasurements(this.options.measurementOptions);
+            }
+        }),
 
-        setRadius: function() {
-            circleSetRadius.apply(this, arguments);
+        onRemove: override(L.Circle.prototype.onRemove, function() {
+            this.hideMeasurements();
+        }, true),
+
+        setLatLng: override(L.Circle.prototype.setLatLng, function() {
             this.updateMeasurements();
-        },
+        }),
+
+        setRadius: override(L.Circle.prototype.setRadius, function() {
+            this.updateMeasurements();
+        }),
 
         formatArea: formatArea,
 
         updateMeasurements: function() {
+            if (!this._measurementLayer) return;
+
             var latLng = this.getLatLng(),
-                formatter = this._measurementOptions.formatDistance || L.bind(this.formatDistance, this);
+                options = this._measurementOptions,
+                formatter = options.formatDistance || L.bind(this.formatDistance, this);
 
             this._measurementLayer.clearLayers();
 
-            if (this._measurementOptions.showArea) {
-                formatter = this._measurementOptions.formatArea || L.bind(this.formatArea, this);
+            if (options.showArea) {
+                formatter = options.formatArea || L.bind(this.formatArea, this);
                 var area = circleArea(this.getRadius());
-                L.marker.measurement(latLng, formatter(area))
+                L.marker.measurement(latLng, formatter(area), options)
                     .addTo(this._measurementLayer);
             }
         }
     })    
+
+    L.Circle.addInitHook(function() {
+        if (this.options.showMeasurements) {
+            this.showMeasurements();
+        }
+    });
 })();
 
